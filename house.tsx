2c313b41e33cf5f7fd7989efa6fd265cc516fe5d
tvyoung <vickiyoung112@gmail.com>
2023-02-19 10:03:53 -0800

    house

---
+++ house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="free_tileset_version_10" tilewidth="32" tileheight="32" tilecount="570" columns="15">
+ <image source="Assets/free_tileset_version_10.png" width="480" height="1216"/>
+</tileset>
