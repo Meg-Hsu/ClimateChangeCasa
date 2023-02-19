3be93ceee9a5dd5a1a39c640c9302cd618674331
tvyoung <vickiyoung112@gmail.com>
2023-02-19 10:10:03 -0800

    title screen

---
+++ startScreen.py
@@ -0,0 +1,156 @@
+# Star Pusher (a Sokoban clone)
+# By Al Sweigart al@inventwithpython.com
+# http://inventwithpython.com/pygame
+# Released under a "Simplified BSD" license
+
+import random, sys, copy, os, pygame
+from pygame.locals import *
+
+
+FPS = 30 # frames per second to update the screen
+WINWIDTH = 800 # width of the program's window, in pixels
+WINHEIGHT = 600 # height in pixels
+HALF_WINWIDTH = int(WINWIDTH / 2)
+HALF_WINHEIGHT = int(WINHEIGHT / 2)
+
+# The total width and height of each tile in pixels.
+TILEWIDTH = 50
+TILEHEIGHT = 85
+TILEFLOORHEIGHT = 40
+
+CAM_MOVE_SPEED = 5 # how many pixels per frame the camera moves
+
+# The percentage of outdoor tiles that have additional
+# decoration on them, such as a tree or rock.
+OUTSIDE_DECORATION_PCT = 20
+
+BRIGHTBLUE = (  0, 170, 255)
+WHITE      = (255, 255, 255)
+BGCOLOR = BRIGHTBLUE
+TEXTCOLOR = WHITE
+
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, IMAGESDICT, TILEMAPPING, OUTSIDEDECOMAPPING, BASICFONT, PLAYERIMAGES, currentImage
+
+    # Pygame initialization and basic set up of the global variables.
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+
+    # Because the Surface object stored in DISPLAYSURF was returned
+    # from the pygame.display.set_mode() function, this is the
+    # Surface object that is drawn to the actual computer screen
+    # when pygame.display.update() is called.
+    DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
+
+    pygame.display.set_caption('Climate Change Casa')
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+
+    # A global dict value that will contain all the Pygame
+    # Surface objects returned by pygame.image.load().
+    ''' IMAGESDICT = {'uncovered goal': pygame.image.load('RedSelector.png'),
+                  'covered goal': pygame.image.load('Selector.png'),
+                  'star': pygame.image.load('Star.png'),
+                  'corner': pygame.image.load('Wall_Block_Tall.png'),
+                  'wall': pygame.image.load('Wood_Block_Tall.png'),
+                  'inside floor': pygame.image.load('Plain_Block.png'),
+                  'outside floor': pygame.image.load('Grass_Block.png'),
+                  'title': pygame.image.load('star_title.png'),
+                  'solved': pygame.image.load('star_solved.png'),
+                  'princess': pygame.image.load('princess.png'),
+                  'boy': pygame.image.load('char1.png'),
+                  'catgirl': pygame.image.load('char2.png'),
+                  'horngirl': pygame.image.load('horngirl.png'),
+                  'pinkgirl': pygame.image.load('pinkgirl.png'),
+                  'rock': pygame.image.load('Rock.png'),
+                  'short tree': pygame.image.load('Tree_Short.png'),
+                  'tall tree': pygame.image.load('Tree_Tall.png'),
+                  'ugly tree': pygame.image.load('Tree_Ugly.png')} '''
+
+    IMAGESDICT = {'boy': pygame.image.load('char1.png'),
+                  'girl': pygame.image.load('char2.png'),
+                  'title': pygame.image.load('startscreen.png')}
+
+    # These dict values are global, and map the character that appears
+    # in the level file to the Surface object it represents.
+    '''TILEMAPPING = {'x': IMAGESDICT['corner'],
+                   '#': IMAGESDICT['wall'],
+                   'o': IMAGESDICT['inside floor'],
+                   ' ': IMAGESDICT['outside floor']}
+    OUTSIDEDECOMAPPING = {'1': IMAGESDICT['rock'],
+                          '2': IMAGESDICT['short tree'],
+                          '3': IMAGESDICT['tall tree'],
+                          '4': IMAGESDICT['ugly tree']}'''
+
+    # PLAYERIMAGES is a list of all possible characters the player can be.
+    # currentImage is the index of the player's current player image.
+    currentImage = 0
+    PLAYERIMAGES = [IMAGESDICT['boy'],
+                    IMAGESDICT['girl']]
+    
+    startScreen()
+
+
+
+
+def startScreen():
+    """Display the start screen (which has the title and instructions)
+    until the player presses a key. Returns None."""
+
+    # Position the title image.
+    titleRect = IMAGESDICT['title'].get_rect()
+    topCoord = 50 # topCoord tracks where to position the top of the text
+    titleRect.top = topCoord
+    titleRect.centerx = HALF_WINWIDTH
+    topCoord += titleRect.height
+
+    # Unfortunately, Pygame's font & text system only shows one line at
+    # a time, so we can't use strings with \n newline characters in them.
+    # So we will use a list with each line in it.
+    instructionText = ['TITLE SCREEN! CLIMATE CHANGE CASA',
+                       'Push the stars over the marks.',
+                       'Arrow keys to move, WASD for camera control, P to change character.',
+                       'Backspace to reset level, Esc to quit.',
+                       'N for next level, B to go back a level.']
+
+    # Start with drawing a blank color to the entire window:
+    DISPLAYSURF.fill(BGCOLOR)
+
+    # Draw the title image to the window:
+    DISPLAYSURF.blit(IMAGESDICT['title'], titleRect)
+
+    # Position and draw the text.
+    for i in range(len(instructionText)):
+        instSurf = BASICFONT.render(instructionText[i], 1, TEXTCOLOR)
+        instRect = instSurf.get_rect()
+        topCoord += 10 # 10 pixels will go in between each line of text.
+        instRect.top = topCoord
+        instRect.centerx = HALF_WINWIDTH
+        topCoord += instRect.height # Adjust for the height of the line.
+        DISPLAYSURF.blit(instSurf, instRect)
+
+    while True: # Main loop for the start screen.
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    terminate()
+                return # user has pressed a key, so return.
+
+        # Display the DISPLAYSURF contents to the actual screen.
+        pygame.display.update()
+        FPSCLOCK.tick()
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
